e770683e14985bdc2a321163176c74d9b85a1768
MarkBorito <markadrianborito30@gmail.com>
2026-09-26 12:07:10 +0800

    Change default page from 'home' to 'landing'

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $pages = [
 ];
 
 //                        change this 'register'. pick the page in the $pages
-$page = $_GET['page'] ?? 'home';
+$page = $_GET['page'] ?? 'landing';
 
 if (
     !is_string($page) ||
